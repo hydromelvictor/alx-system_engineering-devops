4008e1b7338108a060b92f9c8930ea76206470a6
harkinder-dark <victorvaddely@gmail.com>
2022-11-15 04:26:32 -0800

    advanced regexp

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:\+?\w+\] \[to:\+?\d+\] \[flags:-\d:\d:-\d:-\d:-\d\]/).join(",")
+puts ARGV[0].scan(/\[from:(\+?\w+)\] \[to:\+?\d+\] \[flags:-\d:\d:-\d:-?\d:-\d\]/).join(",")
