dd6422209d041213cce2d88d97ad57358df80c5b
harkinder-dark <victorvaddely@gmail.com>
2023-01-10 12:06:51 +0000

    0x0A. configuration mmanagement

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Using Puppet, create a file in /tmp
+file { 'school':
+  ensure  => 'present'
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+puppet language server learning
