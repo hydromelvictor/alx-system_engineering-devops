32cac6afb585634edf66dbfc0e185719a02a5f7f
harkinder-dark <victorvaddely@gmail.com>
2023-01-12 15:57:06 +0000

    server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+#Time to practice configuring your server with Puppet! Just as you did before
+package {
+    'nginx':
+    ensure => installed,
+}
+
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
+}
+
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
+}
+
+service {'nginx':
+    ensure => running,
+}
+
